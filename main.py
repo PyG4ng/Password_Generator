a875ed8218867b083274024ab9851ed8a8fa33d6
PyG4ng <91914909+PyG4ng@users.noreply.github.com>
2021-12-03 16:32:50 +0300

    Adding search feature, data now in .json format

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import random
 import string
+import json
 from tkinter import *
 from tkinter import messagebox
 
 import pyperclip
+
+
+# ---------Search-----------------------------------------------
+def find_password():
+    website = web_entry.get()
+    if website == '':
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found")
+        else:
+            result = data.get(website)
+            if result is None:
+                messagebox.showinfo(title="Error", message="No details for the website exists")
+            else:
+                messagebox.showinfo(title=website, message=f'Email: {result.get("email")}\n'
+                                                           f'Password: {result.get("password")}\n'
+                                                           f'Your password has been copied in the clipboard')
+                pyperclip.copy(result.get("password"))
 
 
 # ---------Password Generator-----------------------------------
@@ -16,18 +39,29 @@ def generate_pass():
             [random.choice(string.ascii_letters),
              random.choice(string.digits),
              random.choice(string.punctuation)])
-        password_generator_result = ''.join([password_generator_result, password_generator])
+        if password_generator != "\\":
+            password_generator_result = ''.join([password_generator_result, password_generator])
     pyperclip.copy(password_generator_result)
     pass_entry.delete(0, END)
     pass_entry.insert(END, password_generator_result)
 
 
 # --------Save Password-----------------------------------------
+def writing_in_json(_file, _data):
+    with open(_file, "w") as _data_file:
+        json.dump(_data, _data_file, indent=4)
+
 
 def save_data():
     website = web_entry.get()
     email = mail_entry.get()
     password = pass_entry.get()
+    new_data = {
+        website.capitalize(): {
+            "email": email,
+            "password": password
+        }
+    }
     if website == '' or email == '' or password == '':
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
     else:
@@ -35,8 +69,18 @@ def save_data():
                                              message=f"Your entries: \nEmail: {email} \n"
                                                      f"Password: {password} \nIf everything is alright press Ok")
         if user_choice:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                # Loading existing data
+                with open("data.json", "r") as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                # Creates a json file to upload new_data
+                writing_in_json("data.json", new_data)
+            else:
+                # updating data
+                data.update(new_data)
+                writing_in_json("data.json", data)
+            finally:
                 web_entry.delete(0, END)
                 pass_entry.delete(0, END)
                 web_entry.focus()
@@ -66,20 +110,23 @@ pass_lbl.grid(row=3, column=0)
 # Entries
 web_entry = Entry(width=40)
 web_entry.focus()
-web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.grid(row=1, column=1)
 
-mail_entry = Entry(width=40)
-mail_entry.insert(END, "mail@mail.com")
+mail_entry = Entry(width=67)
+mail_entry.insert(END, "myemailadress@mail.com")
 mail_entry.grid(row=2, column=1, columnspan=2)
 
-pass_entry = Entry(width=22)
+pass_entry = Entry(width=40)
 pass_entry.grid(row=3, column=1)
 
-# # Buttons
-genpass_btn = Button(text="Generate Password", command=generate_pass)
+# Buttons
+search_btn = Button(text="Search", width=21, command=find_password)
+search_btn.grid(row=1, column=2)
+
+genpass_btn = Button(text="Generate Password", width=21, command=generate_pass)
 genpass_btn.grid(row=3, column=2)
-#
-add_btn = Button(text="Add", width=34, command=save_data)
+
+add_btn = Button(text="Add", width=57, command=save_data)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
